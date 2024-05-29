659270a19cdafd5c9fcbdadd1250072623757489
--unset <azurylldmm@gmail.com>
2024-05-29 00:39:54 -0400

    Added i18n strings for Book of Carols config.

--- Button's Extra Books/Powers/ExtraGifts.cs
+++ Button's Extra Books/Powers/ExtraGifts.cs
@@ -24,7 +24,7 @@ namespace ButtonsExtraBooks.Powers
             }
             catch (Exception ex)
             {
-                Log.Error("Error in ButtonsExtraBooks_ExtraGifts.updateFriendshipGifts_Prefix: \n" + ex);
+                Log.Error("Error in ButtonsExtraBooks_ExtraGifts.updateFriendshipGifts_Postfix: \n" + ex);
             }
         }
     }
